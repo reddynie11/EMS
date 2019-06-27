883f4713cdca701a6f5d64b49f9e22d8d26e89f0
Narasimha Reddy <reddy.nie11@gmail.com>
2019-06-27 19:48:02 +0530

    Login page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 var home = require('./routes/home');
+var login = require('./routes/login');
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
@@ -17,6 +18,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', home);
+app.get('/login',login);
 
 
 
--- routes/login.js
+++ routes/login.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const router = express.Router();
 
+router.use('/',(req,res)=>{
+    res.render('login/');
+});
+
+module.exports = router;
 
